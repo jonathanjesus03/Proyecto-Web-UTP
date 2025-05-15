cd3b1d215b9c2dc5ae9d2c7ab37171b59d73f80d
Jonathan <u22100659@utp.edu.pe>
2025-05-15 15:32:40 -0500

    Subida de screenshots y actualización del proyecto

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,95 +1,96 @@
 //LOGIN
-document.addEventListener("DOMContentLoaded", function() { //Asegurarse de que el DOM esté listo antes de ejecutar el código
-  document.getElementById('login-form').addEventListener('submit', async function(e) {
+document.addEventListener("DOMContentLoaded", function () {
+  //Asegurarse de que el DOM esté listo antes de ejecutar el código
+  document
+    .getElementById("login-form")
+    .addEventListener("submit", async function (e) {
       e.preventDefault();
 
       let usuarios = [];
-      try{
+      try {
         const response = await fetch("/assets/data/datos.json");
         const data = await response.json();
-        data.users.forEach(us => {
-            usuarios.push(us);
-      })}
-      catch(error){
+        data.users.forEach((us) => {
+          usuarios.push(us);
+        });
+      } catch (error) {
         console.error("Error al cargar el JSON:", error);
       }
-      
+
       // Obtener los valores del correo y la contraseña
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
 
       // Definir las credenciales válidas
       let validarEmail;
       let validarPassword;
-      console.log("String fuera del fetch: "+usuarios.length)
-      usuarios.forEach(user => {
+      console.log("String fuera del fetch: " + usuarios.length);
+      for (const user of usuarios) {
         validarEmail = user.email;
         validarPassword = user.password;
         // Validación de las credenciales
         if (email === validarEmail && password === validarPassword) {
-          localStorage.setItem("email", email); 
+          localStorage.setItem("email", email);
           console.log(`${validarEmail}::::${validarPassword}`);
           window.location.href = "/pages/page2.html";
           return;
         }
-      })
-      alert('Credenciales inválidas, intente de nuevo');
+      }
+      alert("Credenciales inválidas, intente de nuevo");
     });
 
   //MOVIMIENTO DE LAS CAJAS
   document.getElementById("a-login").addEventListener("click", login);
   document.getElementById("a-register").addEventListener("click", register);
-  
+
   //Declare variables
   var container_login_register = document.querySelector(".right-side");
-  
+
   var form_login = document.querySelector(".login-form");
   var form_register = document.querySelector(".register-form");
   var rear_box = document.querySelector(".left-side");
-  
+
   weightPage();
   window.addEventListener("resize", weightPage);
-  
-  function weightPage(){
-    if(window.innerWidth > 850){
-        rear_box.style.display = "block";
-    }else{
+
+  function weightPage() {
+    if (window.innerWidth > 850) {
+      rear_box.style.display = "block";
+    } else {
       form_register.style.display = "none";
       form_login.style.display = "block";
-      rear_box.style.display = "block"; 
+      rear_box.style.display = "block";
       rear_box.style.opacity = "1";
       container_login_register.style.left = "0px";
     }
   }
-  
-  function register(){
-    if(window.innerWidth > 850){
-      form_register.style.display = "block"
+
+  function register() {
+    if (window.innerWidth > 850) {
+      form_register.style.display = "block";
       container_login_register.style.left = "410px";
       form_login.style.display = "none";
-      rear_box.style.opacity = "1"; 
-    }else{
+      rear_box.style.opacity = "1";
+    } else {
       form_register.style.display = "block";
       container_login_register.style.left = "0px";
       form_login.style.display = "none";
-      rear_box.style.display = "block"; 
+      rear_box.style.display = "block";
       rear_box.style.opacity = "1";
     }
-    
   }
-  
-  function login(){
-    if(window.innerWidth > 850){
+
+  function login() {
+    if (window.innerWidth > 850) {
       form_register.style.display = "none";
       container_login_register.style.left = "10px";
       form_login.style.display = "block";
-      rear_box.style.opacity = "1"; 
-    }else{
+      rear_box.style.opacity = "1";
+    } else {
       form_register.style.display = "none";
       container_login_register.style.left = "0px";
       form_login.style.display = "block";
-      rear_box.style.display = "block"; 
+      rear_box.style.display = "block";
     }
-  }  
-
+  }
 });
--- assets/script/script4.js
+++ assets/script/script4.js
@@ -1,71 +1,79 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("/assets/data/datos.json") 
-      .then(response => response.json())
-      .then(data => fillForm(data))
-      .catch(error => console.error("Error al cargar el JSON:", error));
+  fetch("/assets/data/datos.json")
+    .then((response) => response.json())
+    .then((data) => fillForm(data))
+    .catch((error) => console.error("Error al cargar el JSON:", error));
+});
+
+document.getElementById("button_save").addEventListener("click", saveChanges);
+document.getElementById("button_edit").addEventListener("click", enableEditing);
+
+function fillForm(data) {
+  let user_final;
+  let correo_user = localStorage.getItem("email");
+  data.users.forEach((user) => {
+    if (user.email == correo_user) {
+      user_final = user;
+    }
   });
 
-document.getElementById("button_save").addEventListener("click",saveChanges);
-document.getElementById("button_edit").addEventListener("click",enableEditing);
+  document.getElementById("name").value = user_final.name || "";
+  document.getElementById("surname").value = user_final.surname || "";
+  document.getElementById("dob").value = user_final.dob || "";
+  document.getElementById("gender").value = user_final.gender || "";
+  document.getElementById("nationality").value = user_final.nationality || "";
+  document.getElementById("phone_number").value = user_final.phone_number || "";
+  document.getElementById("major").value = user_final.major || "";
+  document.getElementById("enrollment_date").value =
+    user_final.enrollment_date || "";
+  document.getElementById("modalidad").value = user_final.modalidad || "";
+  document.getElementById("email").value = user_final.email || "";
+  document.getElementById("span_cycle").innerText =
+    "Cycle - " + user_final.cycle;
+  document.getElementById("tittle_name_student").innerText = user_final.name;
+}
 
-  function fillForm(data) {
-    let user_final;
-    let correo_user= localStorage.getItem("email");
-    data.users.forEach(user => {
-        if(user.email == correo_user){ user_final = user ;}       
-    });
-
-    document.getElementById("name").value = user_final.name || "";
-    document.getElementById("surname").value = user_final.surname || "";
-    document.getElementById("dob").value = user_final.dob || "";
-    document.getElementById("gender").value = user_final.gender || "";
-    document.getElementById("nationality").value = user_final.nationality || "";
-    document.getElementById("phone_number").value = user_final.phone_number || "";
-    document.getElementById("major").value = user_final.major || "";
-    document.getElementById("enrollment_date").value = user_final.enrollment_date || "";
-    document.getElementById("modalidad").value = user_final.modalidad || "";
-    document.getElementById("email").value = user_final.email || "";
-    document.getElementById("span_cycle").innerText = "Cycle - "+user_final.cycle;
-    document.getElementById("tittle_name_student").innerText = user_final.name;
-    }
-
-  function enableEditing() {
-    const inputs = document.querySelectorAll('.form-field input');
-    inputs.forEach(input => input.removeAttribute('readonly'));
-    inputs.forEach(input => input.style.background = 'rgba(255, 207, 207, 0.507)' )  
-    document.querySelector('button[id="button_edit"]').style.display = 'none';
-    document.querySelector('button[id="button_save"]').style.display = 'inline';
+function enableEditing() {
+  const inputs = document.querySelectorAll(".form-field input");
+  inputs.forEach((input) => input.removeAttribute("readonly"));
+  inputs.forEach(
+    (input) => (input.style.background = "rgba(255, 207, 207, 0.507)")
+  );
+  document.querySelector('button[id="button_edit"]').style.display = "none";
+  document.querySelector('button[id="button_save"]').style.display = "inline";
 }
 
 function saveChanges() {
-    const form = document.getElementById('form_student');
-    const formData = new FormData(form);
+  const form = document.getElementById("form_student");
+  const formData = new FormData(form);
 
-    console.log('Datos guardados:', Object.fromEntries(formData.entries()));
+  console.log("Datos guardados:", Object.fromEntries(formData.entries()));
 
-    const inputs = document.querySelectorAll('#form_student input');
-    inputs.forEach(input => input.setAttribute('readonly', true));
-    inputs.forEach(input => input.style.background = 'rgba(218, 168, 168, 0.507)');
-    document.querySelector('button[id="button_edit"]').style.display = 'inline';
-    document.querySelector('button[id="button_save"]').style.display = 'none';
+  const inputs = document.querySelectorAll("#form_student input");
+  inputs.forEach((input) => input.setAttribute("readonly", true));
+  inputs.forEach(
+    (input) => (input.style.background = "rgba(218, 168, 168, 0.507)")
+  );
+  document.querySelector('button[id="button_edit"]').style.display = "inline";
+  document.querySelector('button[id="button_save"]').style.display = "none";
 
-    alert('Información actualizada con éxito.');
+  alert("Información actualizada con éxito.");
 }
 
 // document.getElementById("form_photo").addEventListener("submit", function(event) {
-//     event.preventDefault(); 
-  
+//     event.preventDefault();
+
 //     const formData = new FormData();
 //     const fileInput = document.getElementById("input_photo");
-//     const file = fileInput.files[0]; 
-  
+//     const file = fileInput.files[0];
+
 //     if (!file) {
 //       alert("Please select a file");
 //       return;
 //     }
-  
-//     formData.append("photo", file); 
-  
+
+//     formData.append("photo", file);
+
 //     fetch("/upload", {
 //       method: "POST",
 //       body: formData,
@@ -86,22 +94,83 @@ function saveChanges() {
 //     });
 //   });
 
+document.getElementById("form_photo").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const file = document.getElementById("input_photo").files[0];
+  if (!file) return alert("Please select a file");
 
-  document.getElementById("form_photo").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const file = document.getElementById("input_photo").files[0];
-    if (!file) return alert("Please select a file");
+  const formData = new FormData();
+  formData.append("photo", file);
 
-    const formData = new FormData();
-    formData.append("photo", file);
+  try {
+    const res = await fetch("/upload", { method: "POST", body: formData });
+    const data = await res.json();
+    data.filePath
+      ? (alert("Photo uploaded successfully!"),
+        (document.getElementById("img_photo").src = data.filePath))
+      : alert("Error uploading the photo");
+  } catch {
+    alert("Error uploading the photo");
+  }
+});
 
-    try {
-        const res = await fetch("/upload", { method: "POST", body: formData });
-        const data = await res.json();
-        data.filePath 
-            ? (alert("Photo uploaded successfully!"), document.getElementById("img_photo").src = data.filePath) 
-            : alert("Error uploading the photo");
-    } catch {
-        alert("Error uploading the photo");
-    }
+//Dark Mode
+function DarkMode() {
+  document.getElementsByClassName("main_content")[0].style.background =
+    "#1d1d1d";
+  document.getElementsByClassName("header_titles")[0].style.background =
+    "#1d1d1d";
+  document.getElementsByClassName("container_student")[0].style.background =
+    "#1d1d1d";
+  document.getElementById("logo_utp").src = "/assets/img/utp.svg";
+  document.getElementById("logo_utp").style.width = "175px";
+
+  // Cambiar texto a blanco
+  const textos = document.querySelectorAll(
+    "h3, label.switch, .header_titles span, label"
+  );
+  textos.forEach((el) => (el.style.color = "white"));
+
+  // Cambiar inputs
+  const inputs = document.querySelectorAll(".form-field input");
+  inputs.forEach((input) => {
+    input.style.backgroundColor = "#3b3b3b";
+    input.style.color = "white";
+  });
+}
+function WhiteMode() {
+  document.getElementsByClassName("main_content")[0].style.background =
+    "#ebe9e9";
+  document.getElementsByClassName("header_titles")[0].style.background = "#fff";
+  document.getElementsByClassName("container_student")[0].style.background =
+    "#fff";
+  document.getElementById("logo_utp").src = "/assets/img/UTP_logo2.jpg";
+
+  // Restaurar colores de texto
+  const textos = document.querySelectorAll(
+    "h1, h3, label.switch, .header_titles span, label"
+  );
+  textos.forEach((el) => (el.style.color = "rgb(255, 48, 48)"));
+
+  // Restaurar inputs
+  const inputs = document.querySelectorAll(".form-field input");
+  inputs.forEach((input) => {
+    input.style.backgroundColor = "rgba(218, 168, 168, 0.507)";
+    input.style.color = "#000";
+  });
+}
+
+const button = document.getElementById("switch");
+const label = document.getElementById("span_mode");
+
+button.addEventListener("click", () => {
+  if (label.innerHTML == "Modo claro") {
+    label.textContent = "Modo oscuro";
+    label.style.color = "black";
+    WhiteMode();
+  } else {
+    label.textContent = "Modo claro";
+    label.style.color = "white";
+    DarkMode();
+  }
 });
